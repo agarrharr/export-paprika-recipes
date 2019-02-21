575c4407ac32f55e0789dfbb0244df302839ffea
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2019-02-20 22:40:29 -0700

    Add category names

--- export-recipes.js
+++ export-recipes.js
@@ -3,8 +3,19 @@ const PaprikaApi = require('paprika-api').PaprikaApi;
 const exportRecipes = async (username, password) => {
   const paprikaApi = new PaprikaApi(username, password);
   const recipes = await paprikaApi.recipes();
+  const categories = await paprikaApi.categories();
+  const categoryHash = categories.reduce((hash, category) => ({
+    ...hash,
+    [category.uid]: category
+  }), {});
 
-  return await Promise.all(recipes.map(recipe => paprikaApi.recipe(recipe.uid)));
+  const recipesDetails = await Promise.all(recipes.map(recipe =>
+    paprikaApi.recipe(recipe.uid))
+  );
+  return recipesDetails.map(recipe => ({
+    ...recipe,
+    categories: recipe.categories.map(uid => categoryHash[uid])
+  }));
 }
 
 module.exports = exportRecipes;
